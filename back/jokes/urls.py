5c12944f03dd8d8488540cb323935fff47deee28
Зеркаль Дмитрий <DZerkal@croc.ru>
2026-05-21 00:53:23 +0500

    Add API root route

--- back/jokes/urls.py
+++ back/jokes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import health_check, random_joke, saved_jokes
+from .views import health_check, index, random_joke, saved_jokes
 
 urlpatterns = [
+    path('', index, name='api-index'),
     path('health/', health_check, name='health-check'),
     path('jokes/random/', random_joke, name='random-joke'),
     path('jokes/saved/', saved_jokes, name='saved-jokes'),
