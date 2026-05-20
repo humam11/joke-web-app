056ef56b3c6599ec18e0bb92f3dc332d13a8be23
Зеркаль Дмитрий <DZerkal@croc.ru>
2026-05-21 00:34:42 +0500

    Add authentication panel

--- back/jokes/urls.py
+++ back/jokes/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import health_check, random_joke, saved_jokes
+from .views import auth_login, auth_logout, auth_me, auth_register, health_check, random_joke, saved_jokes
 
 urlpatterns = [
     path('health/', health_check, name='health-check'),
+    path('auth/me/', auth_me, name='auth-me'),
+    path('auth/login/', auth_login, name='auth-login'),
+    path('auth/logout/', auth_logout, name='auth-logout'),
+    path('auth/register/', auth_register, name='auth-register'),
     path('jokes/random/', random_joke, name='random-joke'),
     path('jokes/saved/', saved_jokes, name='saved-jokes'),
 ]
